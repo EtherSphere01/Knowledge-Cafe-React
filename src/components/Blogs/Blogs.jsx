374c7c6a1bc7ca9aadcb8f34f9e7113ca702fec9
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-07 23:38:44 +0600

    first commit

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Blog from "../Blog/Blog";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const data = "blog.json";
+    fetch(data)
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
   return (
     <div>
-      <Blog></Blog>
+      <div>
+        {blogs.map((blog) => (
+          <Blog></Blog>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Blogs;
+
+// github contributions
+// github contributions
+
